6b336eea6c11cc665e9081c5d3f8c7e1cf2769a0
OnTheRocks <nathan_huber@yahoo.com>
2021-12-15 23:58:40 -0600

    Initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import CreateNote from './components/CreateNote';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Notes from './components/Notes';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return(
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/create" element={<CreateNote />} />
+      </Routes>
+    </Router>   
+  )}
 
 export default App;
